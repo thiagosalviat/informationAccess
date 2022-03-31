10cead3fe998a373e57513ac2eedc71995ad5725
Thiago Salviato <thiagosalviat@gmail.com>
2022-03-31 16:11:28 -0300

    add Hashing Security

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const { LoggerLevel } = require('mongodb');
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedfiels: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +38,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username, 
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -54,7 +52,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err,foundUser){
         if(err) {
